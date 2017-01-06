cf7efeb5b653e62e747c1ec4a44407b54275c7c9
Nathanael Araujo <Nathanael.Araujo@CSSP-571.local>
2017-01-06 15:54:56 -0200

    Deleting

--- facebook_messenger/features/support/env.rb
+++
@@ -1,9 +0,0 @@
-if ENV['PLATFORM'] == 'ios'
-  require 'calabash-cucumber/cucumber'
-elsif ENV['PLATFORM'] == 'android'
-  require 'calabash-android/cucumber'
-end
-
-
-require "yaml"
-CONFIG = YAML.load_file("config/dev.yml")
